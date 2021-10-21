53ac46333542fc72c6ec773965e0ea1e9b2e7b38
JamesZam96 <jameszamgm@gmail.com>
2021-10-21 18:08:48 -0500

    Backendprueba1

--- src/app.js
+++ src/app.js
@@ -4,9 +4,13 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 const authRoutes = require('./routes/auth.routes')
+require('dotenv').config()
 
 //configuraciones
 app.set('port',process.env.PORT || 3000)
+mongoose.connect(process.env.DB_STRING)
+.then(db => console.log('Connected to mongo'))
+.catch(err => console.log(err))
 
 //middlewares
 app.use(morgan('dev'))
